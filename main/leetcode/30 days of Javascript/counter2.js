8abcc8a343a53881a426cb96fbed5aaa1e25add5
Anni32 <asheis@gmail.com>
2024-09-20 16:48:11 -0700

    Create counter2.js

---
+++ main/leetcode/30 days of Javascript/counter2.js
@@ -0,0 +1,21 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) { //defines createCounter, which includes methods increment, decrement, and reset
+    var reset = init //creates a variable from the passed value
+    return{
+        increment: function(){
+            init++ //passed value + 1
+             return init
+        },
+        decrement: function(){
+            init-- //passwed value - 1
+             return init
+        },
+        reset: function(){
+            init = reset; //sets init back to inital reset
+             return init //returns init
+        }
+    }
+};
