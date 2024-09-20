e0d820664f845eecb8ed9da20434e8be02b5478e
Anni32 <asheis@gmail.com>
2024-09-20 15:23:41 -0700

    Update tobeornottobe.js

--- main/leetcode/30 days of Javascript/tobeornottobe.js
+++ main/leetcode/30 days of Javascript/tobeornottobe.js
@@ -1,1 +1,22 @@
-
+/**
+ * @param {string|number} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(eq) {
+            if (eq === val) {
+                return true
+                }
+            else{
+                throw new Error('Not Equal')}
+        },
+        notToBe: function(eq){
+            if (eq !== val){
+                return true}
+            else{
+                throw new Error('Equal')
+            }
+            }
+        }
+}
