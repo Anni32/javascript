5921917218975e2c7fda5b8c5c1bf178f55e83d9
Anni32 <asheis@gmail.com>
2024-09-20 11:27:22 -0700

    Create counter.js

---
+++ main/leetcode/30 days of Javascript/counter.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {return function counter(y){return n++}};
+
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
