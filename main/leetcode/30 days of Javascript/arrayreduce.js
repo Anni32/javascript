1b8872df8a332e46472c9e909600c075e3d39546
Anni32 <asheis@gmail.com>
2024-09-26 11:48:10 -0700

    Update arrayreduce.js

--- main/leetcode/30 days of Javascript/arrayreduce.js
+++ main/leetcode/30 days of Javascript/arrayreduce.js
@@ -5,8 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    new1 = 0
-    for (let i = 0; i<nums.length; i++){
-        new1 = fn(new1, nums[i])
-    }return new1+init
+    new1 = 0 //initiates accumated total
+    for (let i = 0; i<nums.length; i++){ //iterates over array
+        new1 = fn(new1, nums[i]) //creates variable that uses the fn () to change items in array
+    }return new1+init // returns modified number + initial value to return total after adding in inital values
 }
