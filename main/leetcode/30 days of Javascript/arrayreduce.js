b34ac142d6ec3899be28ea63d60269442912ddf4
Anni32 <asheis@gmail.com>
2024-09-26 12:23:32 -0700

    Update arrayreduce.js

--- main/leetcode/30 days of Javascript/arrayreduce.js
+++ main/leetcode/30 days of Javascript/arrayreduce.js
@@ -7,11 +7,11 @@
 var reduce = function(nums, fn, init) {
     let new1 = init;
     for (let i = 0; i<nums.length; i++){
-        new1 = fn(new1, nums[i]);
+        new1 = fn(new1, nums[i]); //sets new1 to be the fn() modified new value
     }
-    if (nums.length === 0){
-        return init;
+    if (nums.length === 0){ //if empty array
+        return init; //return init value
     }else{
-        return new1;
+        return new1; //return func modified value
     }
 }
